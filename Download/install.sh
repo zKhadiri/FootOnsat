2e2e7c6445f1499967f0f9b39e1f2706e9130308
zakaria <zakariya-khadiri@hotmail.fr>
2021-04-23 11:38:11 +0000

    First commit

---
+++ Download/install.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+
+if [ -f /etc/apt/apt.conf ] ; then
+    echo "#########################################################"
+	echo "#       this image is not supported                     #"
+	echo "#########################################################"
+    exit 1
+elif [ -f /etc/opkg/opkg.conf ] ; then
+   STATUS='/var/lib/opkg/status'
+fi
+
+if [ -d /usr/lib/python3.8 ] ; then
+   echo "Python3"
+   PYTHON='PY3'
+   SQLITE3='python3-sqlite3'
+   PYSIX='python3-six'
+else
+   echo "Python2"
+   PYTHON='PY2'
+   SQLITE3='python-sqlite3'
+   PYSIX='python-six'
+fi
+
+if grep -q $SQLITE3 $STATUS; then
+    sqlite='Installed'
+fi
+
+if grep -q $PYSIX $STATUS; then
+    six='Installed'
+fi
+
+if grep -q 'alsa-utils-aplay' $STATUS; then
+    aplay='Installed'
+fi
+
+if [ $sqlite = "Installed" -a $six = "Installed" -a $aplay = "Installed" ]; then
+     echo ""
+else
+    echo "=========================================================================="
+    echo "Some Depends Need to Be downloaded From Feeds ...."
+    echo "=========================================================================="
+    echo "Opkg Update ..."
+    echo "========================================================================"
+    opkg update
+    echo "========================================================================"
+    echo " Downloading alsa-utils-aplay ......"
+    opkg install alsa-utils-aplay
+    echo "========================================================================"
+    if [ $PYTHON = "PY2" ]; then 
+        echo "========================================================================"
+        echo " Downloading $SQLITE3 , $PYSIX ......"
+        opkg install python-sqlite3
+        echo "========================================================================"
+        opkg install python-six
+        echo "========================================================================"
+    elif [ $PYTHON = "PY3" ]; then 
+        echo "========================================================================"
+        echo " Downloading $SQLITE3 , $PYSIX ......"
+        opkg install python3-sqlite3
+        echo "========================================================================"
+        opkg install python3-six
+        echo "========================================================================"
+    fi
+
+fi
+
+if grep -q 'alsa-utils-aplay' $STATUS; then
+	echo ""
+else
+	echo "#########################################################"
+	echo "#       alsa-utils-aplay Not found in feed              #"
+	echo "#  Notification sound will not work without alsa aplay  #"
+	echo "#########################################################"
+fi
+
+if grep -q $SQLITE3 $STATUS; then
+	echo ""
+else
+	echo "#########################################################"
+	echo "#       $SQLITE3 Not found in feed                      #"
+	echo "#########################################################"
+    exit 1
+fi
+
+if grep -q $PYSIX $STATUS; then
+	echo ""
+else
+	echo "#########################################################"
+	echo "#       $PYSIX Not found in feed                        #"
+	echo "#########################################################"
+    exit 1
+fi
+
+wget -q "--no-check-certificate" "https://github.com/ziko-ZR1/FootOnsat/blob/main/Download/enigma2-plugin-extensions-footonsat_1.0_all.ipk?raw=true" -O "/tmp/enigma2-plugin-extensions-footonsat_1.0_all.ipk";
+
+opkg install /tmp/enigma2-plugin-extensions-footonsat_1.0_all.ipk
+
+echo ""
+echo "#########################################################"
+echo "#          FootOnsat INSTALLED SUCCESSFULLY             #"
+echo "#                BY ZIKO  & Redouane                    #"
+echo "#########################################################"
+echo "#                Restart Enigma2 GUI                    #"
+echo "#########################################################"
+sleep 2
+killall -9 enigma2
+exit 0
